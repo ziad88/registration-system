bd391e2f6bd84583283bf3dd697d52040324ce57
Yara-M <58529210+Yara-M@users.noreply.github.com>
2019-12-21 21:04:23 +0200

    Courses Student Side

---
+++ CoursesJ.js
@@ -0,0 +1,95 @@
+function Start()
+{
+
+var R = Math.floor(Math.random()*5) ;
+
+switch (R)
+{
+case 0 :
+document.getElementById("Courses1").value = "Mathematics 2" ;
+document.getElementById("Courses2").value = "Logic Design" ;
+document.getElementById("Courses3").value = "Programming 1" ;
+document.getElementById("Courses4").value = "Information System" ;
+document.getElementById("Courses5").value = "Physics 2" ;
+document.getElementById("CoursesId1").value = "MT102" ;
+document.getElementById("CoursesId2").value = "LD108" ;
+document.getElementById("CoursesId3").value = "PG101" ;
+document.getElementById("CoursesId4").value = "IS106" ;
+document.getElementById("CoursesId5").value = "PH110" ;
+document.getElementById("CoursesGrade1").value = "C-" ;
+document.getElementById("CoursesGrade2").value = "A+" ;
+document.getElementById("CoursesGrade3").value = "F" ;
+document.getElementById("CoursesGrade4").value = "B-" ;
+document.getElementById("CoursesGrade5").value = "D" ;
+break ;
+case 1 :
+document.getElementById("Courses1").value = "Art critic" ;
+document.getElementById("Courses2").value = "Tour guide" ;
+document.getElementById("Courses3").value = "Museum curator" ;
+document.getElementById("Courses4").value = "Art journal or art website editor" ;
+document.getElementById("Courses5").value = "Museum administrator" ;
+document.getElementById("CoursesId1").value = "AC102" ;
+document.getElementById("CoursesId2").value = "TG108" ;
+document.getElementById("CoursesId3").value = "MC101" ;
+document.getElementById("CoursesId4").value = "AJW106" ;
+document.getElementById("CoursesId5").value = "MA110" ;
+document.getElementById("CoursesGrade1").value = "D+" ;
+document.getElementById("CoursesGrade2").value = "F" ;
+document.getElementById("CoursesGrade3").value = "A-" ;
+document.getElementById("CoursesGrade4").value = "B" ;
+document.getElementById("CoursesGrade5").value = "C+" ;
+break ;
+case 2 :
+document.getElementById("Courses1").value = "accounting systems" ;
+document.getElementById("Courses2").value = "accounting internship" ;
+document.getElementById("Courses3").value = "income tax for individuals" ;
+document.getElementById("Courses4").value = "ncome tax for corporations" ;
+document.getElementById("Courses5").value = "financial statement analysis" ;
+document.getElementById("CoursesId1").value = "AS102" ;
+document.getElementById("CoursesId2").value = "AI108" ;
+document.getElementById("CoursesId3").value = "TI101" ;
+document.getElementById("CoursesId4").value = "TC106" ;
+document.getElementById("CoursesId5").value = "FSA110" ;
+document.getElementById("CoursesGrade1").value = "A" ;
+document.getElementById("CoursesGrade2").value = "B" ;
+document.getElementById("CoursesGrade3").value = "C-" ;
+document.getElementById("CoursesGrade4").value = "F" ;
+document.getElementById("CoursesGrade5").value = "D-" ;
+break ;
+case 3 :
+document.getElementById("Courses1").value = "Biological Molecules" ;
+document.getElementById("Courses2").value = "Modern Genetics" ;
+document.getElementById("Courses3").value = "Exchange and Transport" ;
+document.getElementById("Courses4").value = "Microbiology and Pathogens" ;
+document.getElementById("Courses5").value = "Classification and Biodiversity" ;
+document.getElementById("CoursesId1").value = "BM102" ;
+document.getElementById("CoursesId2").value = "MG108" ;
+document.getElementById("CoursesId3").value = "ET101" ;
+document.getElementById("CoursesId4").value = "MP106" ;
+document.getElementById("CoursesId5").value = "CB110" ;
+document.getElementById("CoursesGrade1").value = "F" ;
+document.getElementById("CoursesGrade2").value = "B" ;
+document.getElementById("CoursesGrade3").value = "C+" ;
+document.getElementById("CoursesGrade4").value = "A" ;
+document.getElementById("CoursesGrade5").value = "D-" ;
+break ;
+case 4 :
+document.getElementById("Courses1").value = "Marketing" ;
+document.getElementById("Courses2").value = "Professional Communications" ;
+document.getElementById("Courses3").value = "Economics" ;
+document.getElementById("Courses4").value = "Human Resource Management" ;
+document.getElementById("Courses5").value = "Business Management" ;
+document.getElementById("CoursesId1").value = "MK102" ;
+document.getElementById("CoursesId2").value = "PC108" ;
+document.getElementById("CoursesId3").value = "EC101" ;
+document.getElementById("CoursesId4").value = "HRM106" ;
+document.getElementById("CoursesId5").value = "BMG110" ;
+document.getElementById("CoursesGrade1").value = "A+" ;
+document.getElementById("CoursesGrade2").value = "B-" ;
+document.getElementById("CoursesGrade3").value = "C+" ;
+document.getElementById("CoursesGrade4").value = "D" ;
+document.getElementById("CoursesGrade5").value = "F" ;
+break ;
+}
+
+}
